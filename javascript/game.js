3cbfbedc37abe26d34de754972259d18e4d00eaf
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-12 17:14:40 -0400

    barebones

---
+++ javascript/game.js
@@ -0,0 +1,13 @@
+var word = ["Miss Saigon", "Les Mis", "Cabaret", "A Bronx Tale", "Anastasia", "The Addams Family", "Dear Evan Hansen", "Hamilton", "Annie", "The King and I", "Chicago", "Beauty and the Beast", "The Book of Mormon", "Little Shop of Horrors", "The Phantom of the Opera", "Cats", "Once on this Island", "Sweeney Todd", "Hedwig and the Angry Inch", "Matilda the Musical", "Newsies", "Pippin", "The Pirates of Penzance", "Avenue Q", "School of Rock", "Something Rotten"];
+var guesses = document.getElementById("guesses");
+//random word chosen
+
+// underscores of word shown
+
+//player guesses letter
+
+//check if letter was chosen and not number or something
+
+//if correct fill in the black
+
+//else guesses go down
