58c9d07387d9722d1e0ec402cf8c4ff9ac0814b2
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-14 18:46:19 -0400

    using asiic instead of alphabet array

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,4 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var word = ["Miss Saigon", "Les Mis", "Cabaret", "A Bronx Tale", "Anastasia", "The Addams Family", "Dear Evan Hansen", "Hamilton", "Annie", "The King and I", "Chicago", "Beauty and the Beast", "The Book of Mormon", "Little Shop of Horrors", "The Phantom of the Opera", "Cats", "Once on this Island", "Sweeney Todd", "Hedwig and the Angry Inch", "Matilda the Musical", "Newsies", "Pippin", "The Pirates of Penzance", "Avenue Q", "School of Rock", "Something Rotten"];
 var guesses = document.getElementById("guessedletters");
 guesses = [];
@@ -31,14 +31,15 @@ document.onkeyup = function(event) {
      
 
     //check if letter was chosen and not number or something
-    if (~alphabet.indexOf(guess)) {
+    // if (!alphabet.indexOf(guess)) {
+        if (event.keyCode >=65 && event.keyCode <=90) {
             console.log("woohoo you guessed a letter")
             guesses.push(event.key);
             console.log("guesses array: " + guesses);
 
         // check if letter has already been chosen
         //if guess is not last letter in guesses array print new letter chosen
-        if ((guesses.indexOf(guess) + 1) === guesses.length) {
+        if (guesses.indexOf(guess) === guesses.length -1) {
             console.log("new letter chosen");
                 console.log(guesses.indexOf(guess));
                 console.log(guesses.length);
